f643e0015f91b8d0897788f7e17864539137dcff
Alex Castillo <alex.castillo@razortracking.com>
2025-02-11 09:49:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android_kotlin_developer_day3_poo/Basics.kt
@@ -0,0 +1,13 @@
+package com.example.android_kotlin_developer_day3_poo
+
+fun main() {
+    makeCoffee()
+}
+
+fun makeCoffee(){
+    println("Buy beans")
+    println("Grind Beans")
+    println("Heat water to 90°C")
+    println("Add water")
+    println("Push water trough the beans")
+}
